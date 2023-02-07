54f5961e06181bae5ffe21b43b6dc2e37a5d2519
kturenn1 <kturenn1@binghamton.edu>
2023-02-06 22:40:20 -0500

    my commit message

--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,1 +1,20 @@
+import random
 
+#Part A
+weeks = 16
+classes = 5
+tuition = 6000
+cost_per_week = ((tuition / classes) / weeks)
+print("Cost per week:", cost_per_week)
+
+classes_per_week = 7
+cost_per_class = cost_per_week / classes_per_week
+print("The cost of class is", cost_per_class)
+print(classes_per_week, int(classes_per_week))
+print(cost_per_week, int(cost_per_week))
+print(cost_per_class, int(cost_per_class))
+
+
+the_list = [1, 2, 3, 4, 5]
+random_number = random.choice(the_list)
+print(random_number)
