7257608ec6468cf08bec78996f2b1a57b7a7b69e
kturenn1 <kturenn1@binghamton.edu>
2023-03-06 18:11:35 -0500

    bruh

--- ch05/exercises/pyramid.py
+++ ch05/exercises/pyramid.py
@@ -3,11 +3,23 @@ num = int(input("number of stars"))
 def star_pyramid():
     number = num
     star = 0
+
     while number > 0:
         while star <= number:
-            print("*")
+            print(star*'*')
             star += 1
-        print("\n")
         number -= 1
 
 star_pyramid()
+
+def rstar_pyramid():
+    number = num
+    star = num
+
+    while number > 0:
+        while star >= number:
+            print(star*'*')
+            star -= 1
+        number -= 1
+
+rstar_pyramid()
